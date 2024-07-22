ec3927e6d00695c9c26cc503ab5c135e25e405ae
yunuseyvz <yunus.yavuz@campus.lmu.de>
2024-07-22 16:59:45 +0200

    fix routing typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,7 +60,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
+                to="/projects"
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineFundProjectionScreen
